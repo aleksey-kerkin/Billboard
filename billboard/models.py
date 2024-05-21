028bf1d31eff555153f2e1e42deb554f05efd762
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-05-21 23:46:05 +0500

    Redefine models

--- billboard/models.py
+++ billboard/models.py
@@ -5,21 +5,55 @@ from django.urls import reverse
 from django_ckeditor_5.fields import CKEditor5Field
 
 
-class Announcement(models.Model):
+class Category(models.Model):
+    tank = "TN"
+    heal = "HL"
+    dd = "DD"
+    trader = "TR"
+    guild_master = "GM"
+    quest_giver = "QG"
+    blacksmith = "BS"
+    tanner = "TN"
+    alchemist = "AL"
+    spell_master = "SM"
+
     CATEGORIES = [
-        ("Tank", "Танк"),
-        ("Heal", "Хил"),
-        ("DD", "ДД"),
-        ("Trader", "Торговец"),
-        ("Guild Master", "Гильдмастер"),
-        ("Quest Giver", "Квестгивер"),
-        ("Blacksmith", "Кузнец"),
-        ("Tanner", "Кожевник"),
-        ("Alchemist", "Зельевар"),
-        ("Spellmaster", "Мастер заклинаний"),
+        (tank, "Танк"),
+        (heal, "Хил"),
+        (dd, "ДД"),
+        (trader, "Торговец"),
+        (guild_master, "Гильдмастер"),
+        (quest_giver, "Квестгивер"),
+        (blacksmith, "Кузнец"),
+        (tanner, "Кожевник"),
+        (alchemist, "Зельевар"),
+        (spell_master, "Мастер заклинаний"),
     ]
+    name = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=2,
+        choices=CATEGORIES,
+        verbose_name="Название",
+    )
+    subscribers = models.ManyToManyField(
+        User, blank=True, related_name="categories", verbose_name="Подписчики"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Announcement(models.Model):
     title = models.CharField(null=False, blank=False, max_length=100, verbose_name="Заголовок")
-    category = models.CharField(max_length=17, choices=CATEGORIES, verbose_name="Категория")
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="announcements", verbose_name="Категория"
+    )
     text = CKEditor5Field(config_name="default", verbose_name="Текст")
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
@@ -30,6 +64,10 @@ class Announcement(models.Model):
 
     def get_absolute_url(self):
         return reverse("post", args=[str(self.id)])
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
 
 
 class Response(models.Model):
@@ -44,8 +82,3 @@ class Response(models.Model):
 
     def __str__(self):
         return f"Ответ на {self.post.title} от {self.user.username}"
-
-
-# class Subscription(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
-#     subscribed = models.BooleanField(default=False, verbose_name="Подписан")
